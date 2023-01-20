4673d7a88732d83d1574b3a2e92014940fd92f3c
sinka <xgao@zoyoe.com>
2023-01-20 22:26:14 +1100

    integrate with zkWASM-C

---
+++ src/js/g1024.c
@@ -0,0 +1,288 @@
+#include <stdint.h>
+unsigned long long wasm_input(int);
+void require(int);
+
+
+int board[16] = {
+    1,0,1,0,
+    1,1,0,0,
+    1,0,0,1,
+    1,1,0,0,
+};
+
+int currency = 20;
+
+void reward(int k) {
+  if (k>4) {
+    currency = currency + (1 << (k - 4));
+  }
+}
+
+void left(void) {
+  for (int r=0; r<4; r++) {
+    int cur = r*4;
+    int scan = 0;
+
+    /* remove all zeros */
+    for (int i=0; i<4; i++) {
+      int current = r*4+i;
+      if (board[current]!=0) {
+        board[cur] = board[current];
+        cur = cur+1;
+        scan ++;
+      }
+    }
+    for (; scan<4; scan++) {
+        board[cur] = 0;
+        cur = cur+1;
+    }
+
+
+    /* patch pairs */
+    scan = 0;
+    cur = r*4;
+    for (int s=0; s<4; scan++) {
+      int current = r*4 + s;
+      int next = current + 1;
+      if (s!=3 && board[current] == board[next]) {
+        int r = board[current];
+        reward(r);
+        board[cur] = r+1;
+        cur = cur + 1;
+        s = s + 2;
+      } else {
+        board[cur] = board[current];
+        cur = cur + 1;
+        s = s + 1;
+      }
+    }
+    // Fill zero for the rest
+    for (; scan<4; scan++) {
+        board[cur] = 0;
+        cur = cur+1;
+    }
+  }
+}
+
+void right(void) {
+  for (int r=0; r<4; r++) {
+    int scan = 0;
+    int cur = r*4+3;
+
+    /* remove all zeros */
+    for (int i=0; i<4; i++) {
+      int current = r*4+3-i;
+      if (board[current]!=0) {
+        board[cur] = board[current];
+        cur = cur-1;
+        scan ++;
+      }
+    }
+    for (; scan<4; scan++) {
+        board[cur] = 0;
+        cur = cur - 1;
+    }
+
+    /* patch pairs */
+    scan = 0;
+    cur = r*4 + 3;
+    for (int s=3; s>=0; scan++) {
+      int current = r*4+s;
+      int next = current-1;
+      if (s!=0 && board[current] == board[next]) {
+        int r = board[current];
+        reward(r);
+        board[cur] = r+1;
+        cur = cur - 1;
+        s = s - 2;
+      } else {
+        board[cur] = board[current];
+        cur = cur - 1;
+        s = s - 1;
+      }
+    }
+    // Fill zero for the rest
+    for (; scan<4; scan++) {
+        board[cur] = 0;
+        cur = cur - 1;
+    }
+  }
+}
+
+void top(void) {
+  for (int c=0; c<4; c++) {
+
+    int cur = c;
+    int scan = 0;
+
+    /* remove all zeros */
+    for (int i=0; i<4; i++) {
+      int current = i*4 + c;
+      if (board[current]!=0) {
+        board[cur] = board[current];
+        cur = cur+4;
+        scan ++;
+      }
+    }
+    for (; scan<4; scan++) {
+        board[cur] = 0;
+        cur = cur + 4;
+    }
+
+    cur = c;
+    scan = 0;
+    for (int s=0; s<4; scan++) {
+      int current = s*4 + c;
+      int next = current + 4;
+      if (s!=3 && board[current] == board[next]) {
+        int r = board[current];
+        reward(r);
+        board[cur] = r+1;
+        cur = cur + 4;
+        s = s + 2;
+      } else {
+        board[cur] = board[current];
+        cur = cur + 4;
+        s = s + 1;
+      }
+    }
+    // Fill zero for the rest
+    for (; scan<4; scan++) {
+        board[cur] = 0;
+        cur = cur + 4;
+    }
+  }
+}
+
+void bottom(void) {
+  for (int c=0; c<4; c++) {
+    int cur = 12 + c;
+    int scan = 0;
+
+    /* remove all zeros */
+    for (int i=3; i>=0; i--) {
+      int current = i*4 + c;
+      if (board[current]!=0) {
+        board[cur] = board[current];
+        cur = cur-4;
+        scan ++;
+      }
+    }
+    for (; scan<4; scan++) {
+        board[cur] = 0;
+        cur = cur - 4;
+    }
+
+
+    /* patch pairs  */
+    cur = 12 + c;
+    scan = 0;
+    for (int s=3; s>=0; scan++) {
+      int current = s*4 + c;
+      int next = current - 4;
+      if (s!=0 && board[current] == board[next]) {
+        int r = board[current];
+        reward(r);
+        board[cur] = r+1;
+        cur = cur - 4;
+        s = s - 2;
+      } else {
+        board[cur] = board[current];
+        cur = cur - 4;
+        s = s - 1;
+      }
+    }
+    for (; scan<4; scan++) {
+        board[cur] = 0;
+        cur = cur - 4;
+    }
+  }
+}
+
+__attribute__((visibility("default")))
+void randomFill() {
+  int total = 0;
+  int sum = 0;
+  int min = 16;
+  for (int i=0; i<16; i++) {
+    sum += board[i];
+    if (board[i] ==0) {
+      total +=1;
+    } else  if (min > board[i]) {
+      min = board[i];
+    }
+  };
+  if (total==0) {
+    return;
+  }
+  int c = sum%total;
+  //int r = sum%total;
+  for (int i=0; i<16; i++) {
+    if (board[i] == 0) {
+      if (c==0) {
+          board[i] = 1;
+          return;
+      } else {
+        c--;
+      }
+    }
+  }
+}
+
+__attribute__((visibility("default")))
+void setCurrency(int n) {
+  currency = n;
+}
+
+__attribute__((visibility("default")))
+int getCurrency(int n) {
+  return currency;
+}
+
+__attribute__((visibility("default")))
+void sell(int n) {
+  require(n>=0);
+  require(n<16);
+  for (int i=0;i<16;i++) {
+    require(board[i]<=board[n]);
+  }
+  require(board[n] > 0);
+  currency += 1 << (board[n] - 1);
+  board[n] = 0;
+}
+
+__attribute__((visibility("default")))
+void step(int direction) {
+  require(currency > 0);
+  currency--;
+  if(direction == 0) {
+    top();
+  } else if(direction == 1) {
+    left();
+  } else if(direction == 2) {
+    bottom();
+  } else if(direction == 3) {
+    right();
+  }
+}
+
+
+
+__attribute__((visibility("default")))
+void setBoard(int index, int b) {
+  board[index] = b;
+}
+
+__attribute__((visibility("default")))
+int getBoard(int index) {
+  return board[index];
+}
+
+__attribute__((visibility("default")))
+int zkmain() {
+   for (int i=0; i<10; i++) {
+       uint32_t s = (uint32_t)wasm_input(1);
+       step(s);
+   }
+   return 0;
+}

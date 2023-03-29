5107db7bc821725bec2c45507ff3826f5435c4da
rhaoio <richard@rhao.io>
2023-03-29 18:51:06 +1100

    init new styles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import React, { createRef, useState, useEffect, useRef } from "react";
+import "./style.scss";
 import { useAppSelector, useAppDispatch } from "../app/hooks";
 import { loginL1AccountAsync, selectL1Account } from "../data/accountSlice";
+import { addressAbbreviation } from "../utils/address";
+import CurrencyDisplay from "./Currency";
 import {
   Button,
   Container,
@@ -13,9 +16,16 @@ import {
 } from "react-bootstrap";
 
 import logo from "../images/logo.png";
+import Restart from "../images/restart.png";
 
-export function MainNavBar() {
+interface IProps {
+  currency: number;
+  handleRestart: () => void;
+}
+
+export function MainNavBar(props: IProps) {
   const dispatch = useAppDispatch();
+
   let account = useAppSelector(selectL1Account);
 
   useEffect(() => {
@@ -23,25 +33,48 @@ export function MainNavBar() {
   }, []);
 
   return (
-    <Navbar bg="light" expand="lg" style={{ zIndex: "1000" }}>
-      <Navbar.Brand href="http://www.delphinuslab.com">
-        <img src={logo} height="30" alt=""></img>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          {account && (
-            <div className="nav-link">
-              <Navbar.Text> <i className="bi bi-person"></i> Account Connected: {account.address}</Navbar.Text>
-            </div>
-          )}
-          {!account && (
-            <Nav.Link onClick={() => dispatch(loginL1AccountAsync())}>
-              Connect Wallet
-            </Nav.Link>
-          )}
-        </Nav>
-      </Navbar.Collapse>
+    <Navbar expand="lg" style={{ zIndex: "1000" }}>
+      <Container>
+        <Navbar.Brand href="http://www.delphinuslab.com">
+          <img src={logo} height="30" alt="logo"></img>
+        </Navbar.Brand>
+        <Nav.Item className="action-items d-flex">
+          <img
+            src={Restart}
+            height="30"
+            alt="restart"
+            className="me-2 restart-button"
+            onClick={() => props.handleRestart()}
+          ></img>
+          <CurrencyDisplay value={props.currency}></CurrencyDisplay>
+        </Nav.Item>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <div className="divider"></div>
+            {account && (
+              <>
+                <Navbar.Text>
+                  <div>Account</div>
+                  <div>{addressAbbreviation(account.address, 4)}</div>
+                </Navbar.Text>
+              </>
+            )}
+            {!account && (
+              <>
+                <Nav.Link
+                  onClick={() => dispatch(loginL1AccountAsync())}
+                  className="px-2 my-2 py-0"
+                >
+                  Connect Wallet
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
--- src/layout/Main.tsx
+++ src/layout/Main.tsx
@@ -1,9 +1,15 @@
-import React, { createRef, useEffect, useRef, useState } from 'react';
-import { useAppSelector, useAppDispatch } from '../app/hooks';
-import { loadStatus, selectTasks, tasksLoaded, addProvingTask } from "../data/statusSlice";
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import {QRCodeSVG} from 'qrcode.react';
+import React, { createRef, useEffect, useRef, useState } from "react";
+import { useAppSelector, useAppDispatch } from "../app/hooks";
+import {
+  loadStatus,
+  selectTasks,
+  tasksLoaded,
+  addProvingTask,
+} from "../data/statusSlice";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Form } from "react-bootstrap";
+import { QRCodeSVG } from "qrcode.react";
 import initGameInstance from "../js/g1024";
 import History from "../components/History";
 import { NewProveTask } from "../modals/addNewProveTask";
@@ -12,25 +18,37 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 import "./style.scss";
 import "bootswatch/dist/slate/bootstrap.min.css";
-
-import { Container } from 'react-bootstrap';
-import { MainNavBar } from '../components/Nav';
-
+import CurrencyDisplay from "../components/Currency";
+import { Container } from "react-bootstrap";
+import { MainNavBar } from "../components/Nav";
+import One from "../images/1.png";
+import Two from "../images/2.png";
+import Three from "../images/3.png";
+import Four from "../images/4.png";
+import Title from "../images/2048_title.png";
 
 export function Main() {
   const dispatch = useAppDispatch();
-  const [board, setBoard] = useState([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]);
+  const [board, setBoard] = useState([
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+  ]);
   const [focus, setFocus] = useState(-1);
-  const [currency, setCurrency] = useState(40);
+  const [currency, setCurrency] = useState(20);
   const [commands, setCommands] = useState<Array<number>>([]);
+  const [highscore, setHighscore] = useState(20);
   const [submitURI, setSubmitURI] = useState("");
+
+  const [showInputsAsRaw, setShowInputsAsRaw] = useState(false);
   let ready = useAppSelector(tasksLoaded);
 
   function appendCommand(cmds: Array<number>) {
-    setCommands(commands => {return [...commands.concat(cmds)]});
+    setCommands((commands) => {
+      return [...commands.concat(cmds)];
+    });
   }
 
-  function arrowFunction(event:KeyboardEvent) {
+  function arrowFunction(event: KeyboardEvent) {
+    event.preventDefault();
     if (event.key === "ArrowUp") {
       step(0);
     } else if (event.key == "ArrowLeft") {
@@ -43,8 +61,8 @@ export function Main() {
   }
 
   useEffect(() => {
-    initGameInstance().then((ins:any) => {
-      for (var i=0;i<16;i++) {
+    initGameInstance().then((ins: any) => {
+      for (var i = 0; i < 16; i++) {
         board[i] = ins.getBoard(i);
       }
       setBoard([...board]);
@@ -55,40 +73,92 @@ export function Main() {
     return () => {
       document.removeEventListener("keydown", arrowFunction, false);
     };
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    //Set highscore
+    if (currency > highscore) setHighscore(currency);
+  }, [currency]);
 
   function getWitness() {
     let wit = `0x`;
     for (var c of commands) {
       wit = wit + "0" + c.toString(16);
-    };
-    wit = wit +":bytes-packed";
+    }
+    wit = wit + ":bytes-packed";
     return wit;
-
   }
 
   function getURI() {
     let uri = `${commands.length}:i64-0x`;
     for (var c of commands) {
       uri = uri + "0" + c.toString(16);
-    };
-    uri = uri+":bytes-packed";
+    }
+    uri = uri + ":bytes-packed";
     return uri;
-  };
+  }
 
-  async function step(k:number) {
+  function displayCommandIcons() {
+    let icons = [];
+    for (var i = 0; i < commands.length; i++) {
+      let icon = <></>;
+      //Check prev is sell, display as number not arrow
+      if (i > 0) {
+        if (commands[i - 1] === 4 && commands[i - 3] === 4) {
+          //Display cell that has been sold
+          if (commands[i - 1] === 4 || commands[i - 2] != 4) {
+            icon = <span>{commands[i]}</span>;
+            icons.push(icon);
+            continue;
+          }
+        } else if (commands[i - 1] === 4 && commands[i - 2] === 4) {
+          //continue to next to display cell as action (arrow or sell)
+        } else if (commands[i - 1] === 4) {
+          //Display the cell that has been sold
+          icon = <span>{commands[i]}</span>;
+          icons.push(icon);
+          continue;
+        }
+      }
+
+      switch (commands[i]) {
+        case 0:
+          icon = <i className="bi bi-arrow-up mx-1"></i>;
+          break;
+        case 1:
+          icon = <i className="bi bi-arrow-left mx-1"></i>;
+          break;
+        case 2:
+          icon = <i className="bi bi-arrow-down mx-1"></i>;
+          break;
+        case 3:
+          icon = <i className="bi bi-arrow-right mx-1"></i>;
+          break;
+        case 4:
+          icon = <i className="bi bi-cash-stack mx-1"></i>;
+          break;
+
+        default:
+          icon = <span>{commands[i]}</span>;
+      }
+      icons.push(icon);
+    }
+    return icons;
+  }
+
+  async function step(k: number) {
     let ins = await initGameInstance();
     if (ins.getCurrency() == 0) {
-      alert("not enough current to proceed!")
+      alert("not enough current to proceed!");
       return;
     }
     setFocus(-1);
     ins.step(k);
-    for (var i=0;i<16;i++) {
+    for (var i = 0; i < 16; i++) {
       board[i] = ins.getBoard(i);
     }
     setBoard([...board]);
-    setCurrency(ins.getCurrency())
+    setCurrency(ins.getCurrency());
     appendCommand([k]);
   }
 
@@ -98,9 +168,9 @@ export function Main() {
 
   async function sell() {
     let ins = await initGameInstance();
-    if (focus !=-1) {
+    if (focus != -1) {
       let focusValue = ins.getBoard(focus);
-      for (var i=0; i<16; i++) {
+      for (var i = 0; i < 16; i++) {
         let compare = ins.getBoard(i);
         if (compare > focusValue) {
           alert("can only sell highest value block");
@@ -108,68 +178,170 @@ export function Main() {
         }
       }
       ins.sell(focus);
-      for (var i=0;i<16;i++) {
+      for (var i = 0; i < 16; i++) {
         board[i] = ins.getBoard(i);
       }
       setBoard([...board]);
-      setCurrency(ins.getCurrency())
-      appendCommand([4,focus]);
+      setCurrency(ins.getCurrency());
+
+      appendCommand([4, focus]);
       setFocus(-1);
     }
   }
 
-  function cellClass(index:number) {
-    if (board[index] === 0) {
-      return "board-cell"
-    } else {
-      return `board-cell-${board[index]}`
-    }
+  function restartGame() {
+    //reload the window for now
+    window.location.reload();
   }
 
+  function cellClass(index: number) {
+    if (board[index] === 0) {
+      return "board-cell";
+    } else {
+      return `board-cell-${board[index]}`;
+    }
+  }
+  console.log(highscore);
   return (
     <>
-      <MainNavBar></MainNavBar>
-      <Container className="justify-content-center'">
-        <Row className="justify-content-md-center mt-3">
-          <Col lg="2">currency: {currency} </Col>
-          <Col lg="2">total steps: {commands.length} </Col>
-          <Col lg="1"><button onClick={() => sell()}> Sell </button></Col>
+      <MainNavBar currency={currency} handleRestart={restartGame}></MainNavBar>
+      <Container className="justify-content-center mb-4">
+        <Row className="justify-content-md-center  m-auto mt-3 w-50">
+          <Col className="d-flex justify-content-center align-items-center">
+            <img src={Title} height="40px" alt="title" className="me-4" />
+            <CurrencyDisplay
+              tag="Best"
+              value={highscore}
+              className="high-score mx-4"
+            ></CurrencyDisplay>
+            <button onClick={() => sell()} className="sell-button ms-2">
+              Sell
+            </button>
+          </Col>
         </Row>
         <Row className="mt-3">
           <Col>
             <div className="content">
               {[...Array(4)].map((_, r) => {
-                return (<div className="board-row">
-                  {[...Array(4)].map((_, c) => {
-                    let index = r * 4 + c;
-                    return <div className={`${cellClass(r * 4 + c)} board-cell-out`} onClick={() => toggleSelect(r * 4 + c)}>
-                      {
-                        index === focus && <div></div>
-                      }
-                    </div>
-                  })}
-                </div>)
+                return (
+                  <div className="board-row">
+                    {[...Array(4)].map((_, c) => {
+                      let index = r * 4 + c;
+                      return (
+                        <div
+                          className={`${cellClass(r * 4 + c)} board-cell-out`}
+                          onClick={() => toggleSelect(r * 4 + c)}
+                        >
+                          {index === focus && <div></div>}
+                        </div>
+                      );
+                    })}
+                  </div>
+                );
               })}
             </div>
           </Col>
         </Row>
-        <Row className="justify-content-md-center">
-          <Col lg="5" className="mt-3">use keyboard up, down, left, right to play and redeme your price by providing your game playing.</Col>
+        <Row className="game-inputs justify-content-md-center overflow-breakword my-4 py-2 w-50 mx-auto">
+          <Col>
+            <div>
+              <i
+                className="bi bi-eye me-2 cursor-pointer"
+                onClick={() => setShowInputsAsRaw(!showInputsAsRaw)}
+              ></i>
+              <span>
+                {showInputsAsRaw ? "Show Commands" : "Show Raw Proof Inputs"}
+              </span>
+            </div>
+            {showInputsAsRaw ? (
+              <>
+                <div>{getURI()}</div>
+              </>
+            ) : (
+              <>
+                <div>
+                  {commands.length == 0 && "No inputs made yet!"}
+                  {displayCommandIcons().map((icon) => {
+                    return icon;
+                  })}
+                </div>
+              </>
+            )}
+          </Col>
         </Row>
-        <Row className="pt-4">
-          <QRCodeSVG value={getURI()} />
-        </Row>
-        <Row className="justify-content-md-center">
-          <Col lg="5" className="mt-3">{getURI()}</Col>
-          <Col lg="1">
-              <NewProveTask md5="B540BE293CE6C108BCE629BAC91625A4"
-                inputs={`${commands.length}:i64`}
-                witness={getWitness()}
-              ></NewProveTask></Col>
+
+        {commands.length > 0 && (
+          <>
+            <div className="d-flex justify-content-center">
+              <Row className="justify-content-md-center">
+                <Col>
+                  <NewProveTask
+                    md5="B540BE293CE6C108BCE629BAC91625A4"
+                    inputs={`${commands.length}:i64`}
+                    witness={getWitness()}
+                  ></NewProveTask>
+                </Col>
+              </Row>
+            </div>
+          </>
+        )}
+
+        <Row className="justify-content-center">
+          <Col sm={7}>
+            <Row className="pt-4">
+              <Col className="d-flex qr-code">
+                <div>
+                  <QRCodeSVG value={getURI()} />
+                </div>
+
+                <div className="ms-4 qr-msg">
+                  Scan this QR Code to redeem your prize
+                </div>
+              </Col>
+            </Row>
+            <Row className="game-info mt-4">
+              <Row>
+                This game is 99999999 times played. Feel free to share with the
+                friends. Thank You.
+              </Row>
+              <Row>
+                OUR 2048 GAME IS LOGIC & PROBLEM SOLVING GAME AND YOU CAN WIN
+                REAL MONEY WITH IT!
+              </Row>
+              <Row>
+                HOW TO PLAY: Use your arrow keys or W A S D to move the tiles.
+                When two tiles with the same icon touch, they merge into one
+                tile with same icon they summed to one! When you make the
+                highest tile, you could sell the highest tile for currency.
+              </Row>
+              <Row className="my-4">
+                <div className="d-flex align-items-center justify-content-center">
+                  <img src={One} alt="" className="game-icon" />
+                  <i className="bi bi-plus-lg mx-2"></i>
+                  <img src={One} alt="" className="game-icon" />
+                  <i className="bi bi-arrow-right mx-2"></i>
+                  <img src={Two} alt="" className="game-icon" />
+                  <i className="bi bi-plus-lg mx-2"></i>
+                  <img src={Two} alt="" className="game-icon" />
+                  <i className="bi bi-arrow-right mx-2"></i>
+                  <img src={Three} alt="" className="game-icon" />
+                  <i className="bi bi-plus-lg mx-2"></i>
+                  <img src={Three} alt="" className="game-icon" />
+                  <i className="bi bi-arrow-right mx-2"></i>
+                  <img src={Four} alt="" className="game-icon" />
+                  <i className="bi bi-plus-lg mx-2"></i>
+                  <span>...etc</span>
+                </div>
+              </Row>
+              <Row>
+                Game is based on Saming's 2048 and conceptually similar to
+                Threes by Asher Vollmer. Created by ZK wasm
+              </Row>
+            </Row>
+          </Col>
         </Row>
       </Container>
       <History md5="B540BE293CE6C108BCE629BAC91625A4"></History>
     </>
   );
 }
-
